4ffcd535f5b9e79fed3acc48ac1671d2f5496fda
SaaSha Pina <saasha.pina@gmail.com>
2018-09-13 00:45:40 -0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+people = Person.create([{ lastName: 'Monkey', firstName: 'Luffy', middleInitial: 'D', pet: 'Kraken', birthday: '1992-05-05', favoriteColor: 'Red', phoneNumber: '555-555-5555' }, 
+    { lastName: 'Catburglar', firstName: 'Nami', middleInitial: 'C', pet: 'Reindeer/human', birthday: '1991-07-03', favoriteColor: 'Orange', phoneNumber: '555-555-5555' }, 
+    { lastName: 'Roronoa', firstName: 'Zoro', middleInitial: 'H', pet: 'Salamander', birthday: '1990-11-11', favoriteColor: 'Green', phoneNumber: '555-555-5555' },
+    { lastName: 'God', firstName: 'Usopp', middleInitial: 'Y', pet: 'Duck', birthday: '1992-11-11', favoriteColor: 'Green', phoneNumber: '555-555-5555' }])
+#   Character.create(name: 'Luke', movie: movies.first
